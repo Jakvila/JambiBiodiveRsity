97b7e740ab86d7c84806439b98fdea27824eac62
Jakvila <48432845+Jakvila@users.noreply.github.com>
2019-08-19 17:31:20 +0200

    Add files via upload

---
+++ fuzzy_strings.R
@@ -0,0 +1,19 @@
+##################################################################################################
+# Fuzzy string management for C01 2018 wave
+##################################################################################################
+
+setwd("C:/Users/jakob/OneDrive/Desktop/Universidad/R_assignment")
+
+
+Vegetation2018 <- read.csv("VegetationData2018.csv")
+
+table(Vegetation2018$hid)
+
+additionals <-        c("396 (add8)", "407 (add7)", "421 (add6)", "422 (add10)", "431 (add x)", "438 (add13)",
+                        "439 (add9)", "510 (add16)", "520 (add22)", "546 (add12)", "556 (add17)", "568 (add19)",
+                        "593 (add18)", "601 (add14)", "642 (add3)", "643 (add2)", "645 (add4)", "659 (add1)", "697 (add5)")
+
+
+
+
+
